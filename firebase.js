af04819d1e1c653dda8278bf42851f7dbcdfe3bb
Soumojit Shome <121761154+Soumojitshome2023@users.noreply.github.com>
2023-08-06 14:25:28 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -8,15 +8,17 @@ function signUp() {
 
         firebase.auth().createUserWithEmailAndPassword(email, password)
             .then(function (userCredential) {
-                var user = userCredential.user;
                 // console.log("User signed up:", user.uid);
-                sessionStorage.setItem("useruid", user.uid);
-                document.getElementById("auth_status").innerText = "Successful Sign Up";
                 // console.log("User signed up:", userCredential.user);
                 // Add your custom logic after successful sign up
-                document.getElementById("upload_section").style.display = "block";
-                document.getElementById("download_section").style.display = "block";
 
+                var user = userCredential.user;
+                sessionStorage.setItem("useruid", user.uid);
+                document.getElementById("auth_status").innerText = "Successful Sign Up";
+                document.getElementById("upload_section").style.display = "";
+                document.getElementById("download_section").style.display = "";
+                document.getElementById("dlink").innerHTML = "";
+                document.getElementById("your_files_heading").innerHTML = "";
 
             })
             .catch(function (error) {
@@ -24,12 +26,16 @@ function signUp() {
                 // Handle sign up errors
 
                 document.getElementById("auth_status").innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i>User already exists or Wrong`;
+                document.getElementById("upload_section").style.display = "none";
+                document.getElementById("download_section").style.display = "none";
 
             });
 
     }
     else {
         document.getElementById("auth_status").innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i>Enter Correct Email ID & Password`;
+        document.getElementById("upload_section").style.display = "none";
+        document.getElementById("download_section").style.display = "none";
     }
 }
 
@@ -43,25 +49,31 @@ function signIn() {
 
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(function (userCredential) {
-                document.getElementById("auth_status").innerText = "Successful Sign In";
-                var user = userCredential.user;
                 // console.log("User signed up:", user.uid);
-                sessionStorage.setItem("useruid", user.uid);
                 // console.log("User signed in:", userCredential.user);
                 // Add your custom logic after successful sign in
+
+                var user = userCredential.user;
+                sessionStorage.setItem("useruid", user.uid);
+                document.getElementById("auth_status").innerText = "Successful Sign In";
                 document.getElementById("upload_section").style.display = "";
                 document.getElementById("download_section").style.display = "";
+                document.getElementById("dlink").innerHTML = "";
+                document.getElementById("your_files_heading").innerHTML = "";
 
             })
             .catch(function (error) {
                 // console.log("Sign in error:", error);
                 // Handle sign in errors
                 document.getElementById("auth_status").innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i>Wrong Email ID Password`;
-
+                document.getElementById("upload_section").style.display = "none";
+                document.getElementById("download_section").style.display = "none";
             });
     }
     else {
         document.getElementById("auth_status").innerHTML = `<i class="fa-solid fa-triangle-exclamation"></i>Enter Correct Email ID & Password`;
+        document.getElementById("upload_section").style.display = "none";
+        document.getElementById("download_section").style.display = "none";
     }
 }
 
